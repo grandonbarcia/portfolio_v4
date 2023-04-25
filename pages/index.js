905cf2f9f7727cee00096046397d8dbf6984dc8c
Brandon Garcia <grandonleebarcia@gmail.com>
2023-04-25 09:12:50 -0700

    added about me section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import myPic from '../assets/headshot.PNG';
+import myPic2 from '../assets/backshot.jpg';
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <section className="flex justify-center my-auto mx-auto">
+      <section className="flex justify-center my-auto mx-auto mb-8">
         <div>
           <Image
             className="rounded-2xl"
@@ -28,16 +29,44 @@ export default function Home() {
             alt="headshot"
           ></Image>
         </div>
-        <div className="ml-5 w-1/2  grid grid-cols-1 gap-4">
+        <div className="ml-5 grid grid-cols-1 gap-4">
           <p className="text-lg">currently looking for work</p>
           <div>
             <h1 className="text-7xl font-extrabold">Brandon</h1>
             <h1 className="text-7xl font-extrabold">Garcia</h1>
           </div>
-          <p className="text-xl">
+          <p className="text-xl w-5/6">
             is a producct designer who can code. Passionate about learning,
             collaboration, and empowering people.
           </p>
+        </div>
+      </section>
+      <section className="flex justify-end my-auto mx-auto mb-8">
+        <div className="w-1/2 my-auto mx-auto">
+          <h1 className="text-5xl font-extrabold pb-3">About Me</h1>
+          <p className="text-xl pb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p className="text-xl pb-5">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <p className="text-xl">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+        <div className="w-1/3">
+          <Image
+            className="rounded-2xl "
+            src={myPic2}
+            height={400}
+            width={400}
+            alt="headshot"
+          ></Image>
         </div>
       </section>
     </main>
