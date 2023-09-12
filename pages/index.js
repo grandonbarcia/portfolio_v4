0c5a70112dfeb5a0f89c29cf5f50ed1aeb83af04
Brandon Garcia <grandonleebarcia@gmail.com>
2023-09-12 14:13:36 -0700

    increase font size for labeles in form

--- pages/index.js
+++ pages/index.js
@@ -193,9 +193,9 @@ export default function Home() {
         </section>
 
         <section className="text-center mt-32 mb-32   lg:flex lg:justify-between lg:text-left">
-          <div className="lg:w-1/2">
-            <div className="mb-5">
-              <h1 className="text-4xl font-extrabold pb-3">Let's Collab!</h1>
+          <div className="grid lg:w-1/2 ">
+            <div className="">
+              <h1 className="text-4xl font-extrabold ">Let's Collab!</h1>
               <p className="text-xl">grandonleebarcia@gmail.com</p>
             </div>
             <div>
@@ -203,7 +203,9 @@ export default function Home() {
             </div>
             <div className="flex justify-between ">
               <div className="w-1/2">
-                <p className="text-xl lg:w-1/2 lg:text-center">Find Me</p>
+                <p className="text-xl pb-3 font-semibold lg:w-1/2 lg:text-center">
+                  Find Me
+                </p>
                 <div className="flex justify-evenly lg:w-1/2 ">
                   <AiFillGithub size={32} />
                   <AiFillLinkedin size={32} />
@@ -211,25 +213,27 @@ export default function Home() {
                 </div>
               </div>
               <div>
-                <p className="text-xl">San Jose, California </p>
-                <p className="text-lg">+1 (352) 262-7435</p>
+                <p className="text-xl pb-3 font-semibold">
+                  San Jose, California{' '}
+                </p>
+                <p className="text-lg ">+1 (352) 262-7435</p>
               </div>
             </div>
           </div>
           <div className="lg:w-1/3">
-            <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <div className="   mx-auto max-w-screen-md">
               <form action="#" className="space-y-8">
                 <div>
                   <label
                     htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
                   >
                     Your email
                   </label>
                   <input
                     type="email"
                     id="email"
-                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="name@gmail.com"
                     required
                   />
@@ -237,14 +241,14 @@ export default function Home() {
                 <div>
                   <label
                     htmlFor="subject"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
                   >
                     Subject
                   </label>
                   <input
                     type="text"
                     id="subject"
-                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    className="block p-3 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="Let me know how I can help you"
                     required
                   />
@@ -252,14 +256,14 @@ export default function Home() {
                 <div className="sm:col-span-2">
                   <label
                     htmlFor="message"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400"
                   >
                     Your message
                   </label>
                   <textarea
                     id="message"
                     rows="6"
-                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Leave a message..."
                   ></textarea>
                 </div>
