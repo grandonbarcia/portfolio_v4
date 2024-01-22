bb39ac93b029bccea7035b516ff32b4fa1bbb32d
Brandon Garcia <grandonleebarcia@gmail.com>
2024-01-22 13:44:31 -0800

    added email js

---
+++ pages/components/Contact.js
@@ -0,0 +1,101 @@
+'use client';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const Contact = () => {
+  const form = useRef();
+
+  const [isEmailSent, setEmailSent] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_qjtkkq8',
+        'template_66gtj6k',
+        form.current,
+        'FGkYZbHCP05BGpP69'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  function ThankYou() {
+    return <div>Thank You</div>;
+  }
+
+  function Form() {
+    return (
+      <form ref={form} onSubmit={sendEmail} className="space-y-8">
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="name@gmail.com"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            className="block p-3 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="Let me know how I can help you"
+            required
+          />
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400"
+          >
+            Your message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="6"
+            className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Leave a message..."
+          ></textarea>
+        </div>
+        <button
+          href="#_"
+          className="relative inline-block px-4 py-2 font-medium group"
+          type="submit"
+          value="Send"
+        >
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+          <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+          <span className="relative text-black group-hover:text-white">
+            Send Message
+          </span>
+        </button>
+      </form>
+    );
+  }
+
+  return isEmailSent ? <ThankYou /> : <Form />;
+};
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import myPic3 from '../assets/browser.jpg';
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 import { MdWavingHand } from 'react-icons/md';
 import { Maps } from './components/Maps';
+import { Contact } from './components/Contact';
 
 export default function Home() {
   const dot = (
@@ -20,9 +21,9 @@ export default function Home() {
       <div className="hidden xl:block absolute top-0 right-96 ">
         {board.map((dots, index) => {
           return (
-            <ol className="flex">
+            <ol className="flex" key={index}>
               {dots.map((subDots, sIndex) => {
-                return subDots;
+                return <div key={sIndex}>{subDots}</div>;
               })}
             </ol>
           );
@@ -273,62 +274,7 @@ export default function Home() {
           </div>
           <div className="lg:w-1/3">
             <div className="   mx-auto max-w-screen-md">
-              <form action="#" className="space-y-8">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="name@gmail.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="subject"
-                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Subject
-                  </label>
-                  <input
-                    type="text"
-                    id="subject"
-                    className="block p-3 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="Let me know how I can help you"
-                    required
-                  />
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="message"
-                    className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400"
-                  >
-                    Your message
-                  </label>
-                  <textarea
-                    id="message"
-                    rows="6"
-                    className="block p-2.5 w-full text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Leave a message..."
-                  ></textarea>
-                </div>
-                <button
-                  href="#_"
-                  className="relative inline-block px-4 py-2 font-medium group"
-                >
-                  <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                  <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                  <span className="relative text-black group-hover:text-white">
-                    Send Message
-                  </span>
-                </button>
-              </form>
+              <Contact />
             </div>
           </div>
         </section>
