c89ae2d7dbb2f415d5f48b259f4fdf3bf1e4659a
Brandon Garcia <grandonleebarcia@gmail.com>
2024-02-28 14:34:51 -0800

    added Oswald font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Oswald } from '@next/font/google';
+
+const oswald = Oswald({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={oswald.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
